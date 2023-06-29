b60e991e832d8d6f0aeba4ab81f0c9d846389983
haurog <36535774+haurog@users.noreply.github.com>
2023-06-29 10:54:22 +0200

    Get first api example to run

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #[macro_use]
 extern crate rocket;
 
+// add our routes and services modules
+mod routes;
+mod services;
+
+use routes::date::get_current_date;
+// use routes::date::date_plus_month;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn say_hello() -> &'static str {
+    "Hello, welcome to the api!"
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![say_hello, get_current_date]);
 
     Ok(rocket.into())
 }
