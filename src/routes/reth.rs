fe3bcd33d6f8d2aa1b85b5792f0e994165cc6a75
haurog <36535774+haurog@users.noreply.github.com>
2023-06-30 10:16:43 +0200

    First proxy for rETH exchange rates running. Not tested for website yet.

--- src/routes/reth.rs
+++ src/routes/reth.rs
@@ -1,22 +1,12 @@
 use rocket::serde::json::{Json};
-use rocket::serde::{Serialize, Deserialize};
+use rocket::serde::json::{Value};
 
 // import services module
 use crate::services;
 
-// create a struct to hold our Date data
-// need serialize/deserialize to convert to/from JSON
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(crate = "rocket::serde")]
-pub struct Date {
-    pub day: u32,
-    pub month: u32,
-    pub year: i32
-}
-
 // create get-current-date route under /date and call get_current_date service which will return a Date object
 // route returns a Date object converted to JSON
 #[get("/rETH")]
-pub async fn get_reth_exchange_rates() -> Json<Date> {
+pub async fn get_reth_exchange_rates() -> Json<Value> {
     Json(services::reth::get_reth_exchange_rates().await)
 }
--- src/services/reth.rs
+++ src/services/reth.rs
@@ -1,38 +1,17 @@
-// chrono is a time library for Rust
-use chrono::Datelike;
 use reqwest;
 
-use rocket::serde::json::{Value};
+use rocket::serde::json::Value;
 // import our Date object from the routes/date module
-use crate::routes::reth::Date;
 
-pub async fn get_reth_exchange_rates() -> Date {
-    let current_utc = chrono::Utc::now();
-    let year = current_utc.year();
-    let month = current_utc.month();
-    let day = current_utc.day();
-    let current_date = Date {
-        day,
-        month,
-        year
-    };
+pub async fn get_reth_exchange_rates() -> Value {
+    let result = reqwest::get("https://rocketscan.io/api/mainnet/reth/ratios")
+        .await
+        .unwrap()
+        .text()
+        .await;
+    // println!("{:?}", result);
 
-    let result = reqwest::get("https://rocketscan.io/api/mainnet/reth/ratios").await.unwrap().text().await;
-    println!("{:?}", result);
+    let data = rocket::serde::json::from_str::<Value>(result.unwrap().as_str());
 
-
-    return current_date;
-
-
-    // let data = rocket::serde::json::from_str::<Value>(r#"
-    // {
-    //     "name": "John Doe",
-    //     "age": 43,
-    //     "phones": [
-    //         "+44 4743233445",
-    //         "+44 7645345677"
-    //     ]
-    // }"#);
-
-    // data
+    data.unwrap()
 }
