79835dbe665e3ee4e38b843fb995b5387e978439
haurog <36535774+haurog@users.noreply.github.com>
2023-07-03 11:44:33 +0200

    Add permissive CORS header.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 #[macro_use]
 extern crate rocket;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
 
 // add our routes and services modules
 mod routes;
 mod services;
 
-use routes::date::get_current_date;
 use routes::date::date_plus_month;
+use routes::date::get_current_date;
 use routes::reth::get_reth_exchange_rates;
+
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 
 #[get("/")]
 fn say_hello() -> &'static str {
@@ -16,7 +40,16 @@ fn say_hello() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![say_hello, get_current_date, date_plus_month, get_reth_exchange_rates]);
+
+    let rocket = rocket::build().mount(
+        "/",
+        routes![
+            say_hello,
+            get_current_date,
+            date_plus_month,
+            get_reth_exchange_rates
+        ]
+    ).attach(CORS);
 
     Ok(rocket.into())
 }
